37576c570f80cfdc104b1780543c0f30a529dd4b
Pruitt <dtp110020@utdallas.edu>
2016-11-07 16:51:14 -0600

    Updates to the knob task so that it displays end-of-session messages.

--- MotoTrak/MotoTrakPythonCode/PythonPullStageImplementation.py
+++ MotoTrak/MotoTrakPythonCode/PythonPullStageImplementation.py
@@ -211,7 +211,7 @@ class PythonPullStageImplementation (IMotorStageImplementation):
                 PythonPullStageImplementation.Force_Threshold_List.append(current_hit_threshold)
 
                 #If this is an adaptive stage, then display the hit threshold of the current trial in the "end-of-trial" message to the user
-                if stage.StageParameters[hit_threshold_parameter_name].AdaptiveThresholdType is MotorStageAdaptiveThresholdType.Median:    
+                if stage.StageParameters[hit_threshold_parameter_name].ParameterType == MotorStageParameter.StageParameterType.Variable:
                     msg += "(Hit threshold = " + Math.Floor(current_hit_threshold).ToString() + " grams)"
             
             return msg
